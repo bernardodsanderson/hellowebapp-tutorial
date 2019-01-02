0ec3358098c11e946d486b21737037f2ee3e85ef
Bernardo Anderson <bernardodsanderson@gmail.com>
2019-01-02 15:06:20 -0600

    Location 1044

---
+++ collection/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+# import mode
+from collection.models import Thing
+
+# Register your models here.
+class ThingAdmin(admin.ModelAdmin):
+  model = Thing
+  list_display = ('name', 'description',)
+  prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Thing, ThingAdmin)
