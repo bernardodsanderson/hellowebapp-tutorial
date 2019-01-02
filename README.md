51d3bbb3dfc04965c34fb6376b8255f39f94b191
Bernardo Anderson <bernardodsanderson@gmail.com>
2019-01-02 15:49:55 -0600

    Location 1236, Adding a Registration Page

---
+++ README.md
@@ -0,0 +1,5 @@
+Setup: https://hellowebbooks.com/setup/
+
+Urls: https://github.com/hellowebbooks/hellowebapp-code/blob/master/hellowebapp/urls.py
+
+Other tutorial: https://www.youtube.com/watch?v=F5mRW0jo-U4&t=452s
--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from collection.models import Thing
+from collection.forms import ThingForm
 
 # Create your views here.
 def index(request):
@@ -12,4 +13,26 @@ def thing_detail(request, slug):
   thing = Thing.objects.get(slug=slug)
   return render(request, 'things/thing_detail.html', {
     'thing': thing,
+  })
+
+def edit_thing(request, slug):
+  # grab the object
+  thing = Thing.objects.get(slug=slug)
+  # set the form we're using
+  form_class = ThingForm
+  # if we're coming to this view from a submitted form
+  if request.method == 'POST':
+    # grab the data from the submitted form and apply to the form
+    form = form_class(data=request.POST, instance=thing)
+    if form.is_valid():
+      # save the data
+       form.save()
+       return redirect('thing_detail', slug=thing.slug)
+  # otherwise just create the form
+  else:
+    form = form_class(instance=thing)
+  # and render the template
+  return render(request, 'things/edit_thing.html', {
+    'thing': thing,
+    'form': form,
   })
