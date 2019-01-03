6493b66c8af4b6ff49c41dcd802c2510c24e6f70
Bernardo Anderson <bernardodsanderson@gmail.com>
2019-01-02 21:49:31 -0600

    11, Associating Users with Objects

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Setup: https://hellowebbooks.com/setup/
 
 Urls: https://github.com/hellowebbooks/hellowebapp-code/blob/master/hellowebapp/urls.py
 
-Other tutorial: https://www.youtube.com/watch?v=F5mRW0jo-U4&t=452s
+Upgrading to Django 2.1: https://discuss.hellowebapp.com/t/django-2-1-changes/618
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -14,16 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from collection import views
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
-    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
-    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
-    path('things/<slug>/', views.thing_detail, name='thing_detail'),
-    path('things/<slug>/edit/', views.edit_thing, name='edit_thing'),
-    path('admin/', admin.site.urls),
+    url(r'^$', views.index, name='home'),
+    url(r'^about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail, name='thing_detail'),
+    url(r'^things/(?P<slug>[-\w]+)/edit/$', views.edit_thing, name='edit_thing'),
+    url(r'^accounts/password/reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name="password_reset"),
+    url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name="password_reset_done"),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name="password_reset_confirm"),
+    url(r'^accounts/password/done/$', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name="password_reset_complete"),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
